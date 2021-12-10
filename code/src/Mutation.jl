778eed45ad8697df3c5e65fb200f7bc8b9f1a53d
Andreas Rohrbach <skuddeliwoo@gmail.com>
2021-12-10 10:26:39 +0100

    add documentation for Mutation.jl

--- code/src/Mutation.jl
+++ code/src/Mutation.jl
@@ -3,6 +3,7 @@ module Mutation
 using ..Types
 include("./Params.jl")
 
+# update interaction matrix according to Eq. 3, then normalise.
 function mutate(ecosystem::Ecosystem, env)
     # (Eq. 3)
     V = ((m ./ env) * g * μ .* x) * x'
@@ -10,20 +11,15 @@ function mutate(ecosystem::Ecosystem, env)
     M = ecosystem.Ω + V
 
     # normalise Ω
-    while true
-        print("normalising...")
-        M2 = copy(M)
-        row_norm(M2)
-        col_norm(M2)
-        if (sum((M2 - M)^2) < 1e-5)
-            break
-        end
-        M = copy(M2)
-    end
+    normalise(M)
+
+    ecosystem.Ω = copy(M)
 
     return ecosystem
 end
 
+# apply row- and col_normalisation until squared difference of matrix converges
+# below 10^-5
 function normalise(M)
     while true
         println("normalising...")
@@ -38,10 +34,13 @@ function normalise(M)
     end
 end
 
+# normalisation of interaction matrix:
+# entries in the diagonal (self interactions) remain unchanged. the rest of the
+# rows (or cols) are beeing normalised (getting devided by their sum)
 function row_norm(M)
     for i in 1:size(M, 1)
         div = sum(M[i,:]) - M[i,i]
-        
+
         M[i,1:(i-1)] = M[i,1:(i-1)] ./ div
         M[i,(i+1):end] = M[i,(i+1):end] ./ div
     end
@@ -50,7 +49,7 @@ end
 function col_norm(M)
     for i in 1:size(M, 2)
         div = sum(M[:,i]) - M[i,i]
-        
+
         M[1:(i-1),i] = M[1:(i-1),i] ./ div
         M[(i+1):end,i] = M[(i+1):end,i] ./ div
     end
