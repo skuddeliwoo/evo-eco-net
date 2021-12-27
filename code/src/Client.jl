4a90c9013cbe5b6bd60b0170a8fc06c20204a2cf
Andreas Rohrbach <skuddeliwoo@gmail.com>
2021-12-27 10:36:01 +0100

    add LV iteration

--- code/src/Client.jl
+++ code/src/Client.jl
@@ -3,25 +3,25 @@ include("./Params.jl")
 using .Types
 import .Environment
 # import .Development
-# import .Mutation
+import .Mutation
 
 for episode in 1:nEpisodes
-    if (episode % 100 == 0)
-        println("status: episode $episode of $nEpisodes")
-    end
+    # if (episode % 100 == 0)
+    println("status: episode $episode of $nEpisodes")
+    # end
 
     # init environment
     env = Environment.changeEnv(episode)
 
-    # evolutionary chage
-
-    # normalise
+    # apply evolutionary chage & normalise
+    Mutation.mutate(eco, env)
 
     for ecoStep in 1:nEcoSteps
+        println("ecoStep $ecoStep")
         # let ecosystem play out
-
         # L-V model
+        xdot = m * (eco.x ./ env) + env + eco.Ω * eco.x
+        eco.x = eco.x + xdot
     end
-
 
 end
