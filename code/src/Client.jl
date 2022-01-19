fb8f11314111656714ce2ee375e84a3d42745834
Andreas Rohrbach <skuddeliwoo@gmail.com>
2022-01-19 08:23:19 +0100

    cleanup Client

--- code/src/Client.jl
+++ code/src/Client.jl
@@ -2,26 +2,28 @@ include("./Params.jl")
 
 using .Types
 import .Environment
-# import .Development
 import .Mutation
 
 for episode in 1:nEpisodes
-    # if (episode % 100 == 0)
-    println("status: episode $episode of $nEpisodes")
-    # end
+    if (episode % 100 == 0)
+        println("#################################")
+        println("status: episode $episode of $nEpisodes")
+    end
 
     # init environment
     env = Environment.changeEnv(episode)
 
-    # apply evolutionary chage & normalise
-    Mutation.mutate(eco, env)
-
     for ecoStep in 1:nEcoSteps
-        println("ecoStep $ecoStep")
+        # apply evolutionary chage & normalise
+        Mutation.mutate(eco, env)
+
         # let ecosystem play out
         # L-V model
-        xdot = m * (eco.x ./ env) + env + eco.Ω * eco.x
+        xdot = m * (eco.x ./ env) .* (env + eco.Ω * eco.x)
+
         eco.x = eco.x + xdot
+
     end
+    push!(ecoDump, [copy(eco.x), copy(eco.Ω)])
 
 end
