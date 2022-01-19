97814b56ee100b237b3056e2be2576ce91f9a269
Andreas Rohrbach <skuddeliwoo@gmail.com>
2022-01-19 08:22:18 +0100

    add comments, add ecoDump for debugging

--- code/src/Params.jl
+++ code/src/Params.jl
@@ -1,24 +1,29 @@
 using .Types
 
-nModules = 10
-nModuleSize = 5
+nModules = 2
+nModuleSize = 3
 N = nModules * nModuleSize # Number of species # lit: 400
+
+k0 = 10 # base carrying capacity for all species
+kInc = 0.1 # increment / decrement of carying capacities # lit: 0.1
+m = 0.5 # growth rate of all species # lit: 0.5 for all
+g = 0.01 # constant of proportionality in selection-limited evolution # lit: 0.01
+μ = 1e-5 # beneficial mutation rate # lit: 10^-5
+ω = -0.2 # initial interaction strength
 
 # interaction coeffs are init to -0.2; and -1 for diagonal (self interactions)
 # if perf is decent, this can be inc to Float64 (which is default)
-species = ones(Float64, N) / N
-interactions = ones(Float64, N,N) * -0.2
+species = ones(Float64, N) * 0.1
+# species = species / sum(species)
+interactions = ones(Float64, N,N) * ω
 for i in 1:N
     interactions[i,i] = -1
 end
 
 eco = Ecosystem(species, interactions)
 
-k0 = 10 # base carrying capacity for all species
-kInc = 0.1 # increment / decrement of carying capacities
-m = 0.5 # growth rate of all species # lit: 0.5 for all
-g = 0.01 # constant of proportionality in selection-limited evolution
-μ = 1e-5 # beneficial mutation rate # lit: 10^-5
+ecoDump = []
+push!(ecoDump, [copy(eco.x), copy(eco.Ω)])
 
-nEpisodes = 1000 # lit: ?
-nEcoSteps = 50 # lit: 5000
+nEpisodes = 800 # lit: 800
+nEcoSteps = 5000 # lit: 5000
